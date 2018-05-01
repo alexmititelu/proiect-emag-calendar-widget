3832d1c9894089007b8126917f8de28eaeb95ad7
alex.mititelu <blbla@yahoo.com>
2018-05-01 23:04:16 +0300

    Logics

--- calendar.js
+++ calendar.js
@@ -2,21 +2,30 @@
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; 
     let days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
     let currentDate = new Date();
+    currentDayShown = false;
+    currentDayValue = currentDate.getDay();
 
     span = document.getElementById("monthDisplayed");
     txt = document.createTextNode(months[currentDate.getMonth()-1] + " " + currentDate.getFullYear());
     span.appendChild(txt);
-
-            
+        
     currentMonth = months.indexOf(document.getElementById("monthDisplayed").firstChild.nodeValue.split(" ")[0]);
     currentYear = document.getElementById("monthDisplayed").firstChild.nodeValue.split(" ")[1];
+
     resetCalendarHeader();
     
     function resetCalendarHeader() {
         span.firstChild.nodeValue = months[currentMonth] + " " + currentYear;
         resetCalendarContent();
+        currentDayShown = markCurrentDay();
     }
 
+    function markCurrentDay() {
+        if(currentMonth == months.indexOf(currentDate.getMonth()) && currentYear == currentDate.getFullYear()) {
+            return true;
+        }
+        return false;
+    }
     function switchToNextMonth() { 
         if(currentMonth<11) {
             currentMonth++;
@@ -60,42 +69,84 @@
         fifthRow = daysRepresentation.children[4];
         sixthRow = daysRepresentation.children[5];
 
-        indexOfLastDayOfTheMonth = getLastDayOfTheCurrentMonth();
+        indexOfLastDayOfTheMonth = getLastDayOfMonth(currentMonth,currentYear);
+        
+        if(currentMonth>0) {
+        lastDayOfPreviousMonth = getLastDayOfMonth(currentMonth-1,currentYear);
+        } else {
+            lastDayOfPreviousMonth = getLastDayOfMonth(11,currentYear-1);
+        }
+        
+        for(i=indexOfFirstDayOfTheMonth-1;i>=0;i--) {
+            firstRow.children[i].textContent = lastDayOfPreviousMonth;
+            lastDayOfPreviousMonth--;
+            firstRow.children[i].style.opacity = "0.3";
+        }
 
         dayValue = 1;
 
-        for(i=0;i<indexOfFirstDayOfTheMonth;i++) {
-            firstRow.children[i].textContent = null;
-        }
+        // for(i=0;i<indexOfFirstDayOfTheMonth;i++) {
+        //     firstRow.children[i].textContent = null;
+        // }
 
         for(i=indexOfFirstDayOfTheMonth;i<7;i++) {
             firstRow.children[i].textContent = dayValue;
+            if(currentDayShown == true && currentDayValue == dayValue) {
+                firstRow.children[i].style.border = '2px solid blue';
+                firstRow.children[i].style.fontWeight = 'bold';    
+            }
+            firstRow.children[i].style.opacity = "1";
             dayValue++;
         }
 
         for(i=0;i<7;i++) {
             secondRow.children[i].textContent = dayValue;
+            if(currentDayShown == true && currentDayValue == dayValue) {
+                secondRow.children[i].style.border = '2px solid blue';
+                secondRow.children[i].style.fontWeight = 'bold';    
+            }
+            secondRow.children[i].style.opacity = "1";
             dayValue++;
         }
 
         for(i=0;i<7;i++) {
             thirdRow.children[i].textContent = dayValue;
+            if(currentDayShown == true && currentDayValue == dayValue) {
+                thirdRow.children[i].style.border = '2px solid blue';
+                thirdRow.children[i].style.fontWeight = 'bold';    
+            }
+            thirdRow.children[i].style.opacity = "1";
             dayValue++;
         }
         for(i=0;i<7;i++) {
             fourthRow.children[i].textContent = dayValue;
+            if(currentDayShown == true && currentDayValue == dayValue) {
+                fourthRow.children[i].style.border = '2px solid blue';
+                fourthRow.children[i].style.fontWeight = 'bold';    
+            }
+            fourthRow.children[i].style.opacity = "1";
             dayValue++;
         }
 
         lastIndexWrote = -1;
         for(i=0;i<7 && dayValue<=indexOfLastDayOfTheMonth;i++) {
             fifthRow.children[i].textContent = dayValue;
+            if(currentDayShown == true && currentDayValue == dayValue) {
+                fifthRow.children[i].style.border = '2px solid blue';
+                fifthRow.children[i].style.fontWeight = 'bold';    
+            }
             dayValue++;
+            fifthRow.children[i].style.opacity = "1";
             lastIndexWrote++;
         }
         if(dayValue>indexOfLastDayOfTheMonth) {
+
+            dayOfNextMonth = 1;
+
             for(i=lastIndexWrote+1;i<7;i++) {
-                fifthRow.children[i].textContent = null;
+                fifthRow.children[i].textContent = dayOfNextMonth;
+                fifthRow.children[i].style.opacity = "0.3";
+                dayOfNextMonth++;
             }
 
             for(i=0;i<7;i++) {
@@ -109,28 +160,41 @@
             lastIndexWrote = -1;
             for(i=0;i<7 && dayValue<=indexOfLastDayOfTheMonth;i++) {
                 sixthRow.children[i].textContent = dayValue;
+                if(currentDayShown == true && currentDayValue == dayValue) {
+                    sixthRow.children[i].style.border = '2px solid blue';
+                    sixthRow.children[i].style.fontWeight = 'bold';    
+                }
+                sixthRow.children[i].style.opacity = "1";
                 dayValue++;
                 lastIndexWrote++;
             }
             
-            for(i=lastIndexWrote;i<7;i++) {
-                sixthRow.children[i].textContent = null;
+            dayOfNextMonth = 1;
+
+            for(i=lastIndexWrote+1;i<7;i++) {
+                sixthRow.children[i].textContent = dayOfNextMonth;
+                sixthRow.children[i].style.opacity = "0.3";
+                dayOfNextMonth++;
             }
+
+            // for(i=lastIndexWrote;i<7;i++) {
+            //     sixthRow.children[i].textContent = null;
+            // }
         }
     } 
 
 
-    function getLastDayOfTheCurrentMonth() {
+    function getLastDayOfMonth(month,year) {
         fullMonths = ["January", "March", "May", "July", "August", "October", "December"];
         semiFullMonths = ["April","June","September","November"];
         lastDay = 0;
         [0,3,5,7,8,10,12]
-        if(fullMonths.indexOf(months[currentMonth]) != -1) {
+        if(fullMonths.indexOf(months[month]) != -1) {
             lastDay = 31;
-        } else if(semiFullMonths.indexOf(months[currentMonth]) != -1) {
+        } else if(semiFullMonths.indexOf(months[month]) != -1) {
             lastDay = 31;
         } else {
-            if(currentYear%4==0 && currentYear%100 !=0 || currentYear%400) {
+            if(year%4==0 && year%100 !=0 || year%400) {
                 lastDay = 29;
             } else {
                 lastDay = 28;
